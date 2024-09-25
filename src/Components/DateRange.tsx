cc077aefd6264c791c26ade9b1d504bc034d671f
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2024-09-25 14:15:17 -0300

    css base

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -6,19 +6,17 @@ const DateRange = () => {
   const [final, setFinal] = useState("");
 
   return (
-    <form onSubmit={(e) => e.preventDefault}>
+    <form className="box flex" onSubmit={(e) => e.preventDefault}>
       <DateInput
         label={"Inicio"}
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
-      {inicio}
       <DateInput
         label={"Final"}
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
-      {final}
     </form>
   );
 };
